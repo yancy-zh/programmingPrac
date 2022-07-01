7085ecd702c8d80e8aa8c1ae3037683c0042fcc3
Yao Zhang <60941643+yancy-zh@users.noreply.github.com>
2022-07-01 18:35:18 +0800

    added solutions

--- src/demos/stringDevideBy8.java
+++ src/demos/stringDevideBy8.java
@@ -2,7 +2,7 @@ package demos;
 
 import java.util.Scanner;
 
-public class stringDevideBy8
+public class StringDevideBy8
 {
 	
 	public static void main(String[] args)
@@ -38,5 +38,30 @@ public class stringDevideBy8
 				System.out.println(str.substring(i * 8, i * 8 + 8));
 			}
 		}
+		
+	}
+	public static String[] stringDivideBy8(String str)
+	{
+		int len = str.length();
+		// check if the str len is dividable by 8
+		if (len % 8 != 0)
+		{
+			int remainder = len % 8;
+			int numZeroToPad = 8 - remainder;
+			StringBuilder sb = new StringBuilder(str);
+			for (int i = 0; i < numZeroToPad; i++)
+			{
+				sb.insert(i, "0");
+			}
+			str = sb.toString();
+		}
+		// cut the string by 8
+		int numSection = str.length() / 8;
+		String[] strArr = new String[numSection];
+		for (int i = 0; i < numSection; i++)
+		{
+			strArr[i] = (str.substring(i * 8, i * 8 + 8));
+		}
+		return strArr;
 	}
 }
