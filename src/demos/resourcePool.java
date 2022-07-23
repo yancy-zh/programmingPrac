867163f4c3bb21f646c372fc56549fd7ff37d595
yzhangObjectiveIT <yzhang@objectiveit.de>
2022-07-23 20:31:39 +0800

    Create resourcePool.java

---
+++ src/demos/resourcePool.java
@@ -0,0 +1,18 @@
+package demos;
+
+import java.util.Scanner;
+
+public class resourcePool
+{
+	
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNext())
+		{
+			
+		}
+	}
+	
+	
+}
